b09568bd58b163ed6f324b3725cca106204af943
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 18:21:34 +0300

    Initial Commit

---
+++ LinkView/LinkViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkViewApp.swift
+//  LinkView
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LinkViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
